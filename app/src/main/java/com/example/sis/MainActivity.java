fecba93ab4f933fd63ab63f9064f20206062dc1b
Donat Dzheladze <donatdzeladze@gmail.com>
2021-06-08 23:21:11 +0300

    заработал qr от ввода в поле

--- app/src/main/java/com/example/sis/MainActivity.java
+++ app/src/main/java/com/example/sis/MainActivity.java
@@ -2,13 +2,55 @@ package com.example.sis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class MainActivity extends AppCompatActivity {
+
+    // инициализация переменнных
+    EditText qrInput;
+    Button qrButton;
+    ImageView qrOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        qrInput = findViewById(R.id.qr_input);
+        qrButton = findViewById(R.id.btn_qr);
+        qrOutput = findViewById(R.id.qr_output);
+        qrButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // забираем значение из поля ввода для генерации
+                String genText = qrInput.getText().toString().trim();
+                MultiFormatWriter writer = new MultiFormatWriter();
+                try {
+                    BitMatrix matrix = writer.encode(genText, BarcodeFormat.QR_CODE, 350, 350);
+                    BarcodeEncoder encoder = new BarcodeEncoder();
+                    Bitmap bitmap = encoder.createBitmap(matrix);
+                    qrOutput.setImageBitmap(bitmap);
+                    InputMethodManager manager = (InputMethodManager) getSystemService(
+                            Context.INPUT_METHOD_SERVICE
+                    );
+                    manager.hideSoftInputFromWindow(qrInput.getApplicationWindowToken(), 0);
+
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
